a123ddae6e8b58b948a4f8c61f9fd917ed0f64f5
Tony <Tony.mokhtar16@gmailcom>
2020-11-16 18:45:20 +0100

    navbaar toegevoegd

---
+++ resources/views/pages/products.blade.php
@@ -0,0 +1,7 @@
+@include('templates/header')
+<body>
+<div class="overlay-navigation">
+@include('templates/nav')
+</body>
+<script type="text/javascript" src="{{asset('js/nav.js') }}"></script>
+</html>
