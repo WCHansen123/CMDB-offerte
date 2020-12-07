1fc09040d8cdc1cc8e0ba1d39fd5ad227b780a1e
Theus <42929098+TheusProme@users.noreply.github.com>
2020-12-07 10:53:56 +0100

    Mainfrom public -> Dev (#29)
    * Pull Public Dev -> MainfromPublic 7-12-20 (#28)
    
    * Add or update the App Service deployment workflow configuration from Azure Portal.
    
    * Dev -> Main (#27)
    
    * UserRole Update
    
    Role submitted
    - Admin
    - Customer
    
    - Role Check Submitted
    
    * Route toegevoegd op de Product.Index voor individuele links naar Product.Show
    
    Co-authored-by: TonyCheeki <42928817+TonyCheeki@users.noreply.github.com>
    Co-authored-by: Tony <Tony.mokhtar16@gmailcom>
    
    Co-authored-by: Wilco Hansen <34767610+WCHansen123@users.noreply.github.com>
    Co-authored-by: Wilco <wilco.hansen@outlook.com>
    
    Co-authored-by: Azure App Service <donotreply@microsoft.com>

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,9 +6,9 @@
         <div class="row d-flex justify-content-center g-1">
         @foreach($products as $product)
             <div class="col-md-4">
-                <div class="product text-center"> <img src="/img/product_frame.png" width="250">
+                <div class="product text-center"> <img src="/img/{{$product->thumbnail}}" width="250">
                     <div class="about text-left px-3">
-                        <h4>{{$product->name}}</h4> <span class="text-muted">{{$product->description}}</span>
+                        <h4><a href="{{route('products.show', ['product' => $product])}}">{{$product->name}}</a></h4> <span class="text-muted">{{$product->description}}</span>
                         <h3>€{{$product->price}}</h3>
                     </div>
                         <a href="{{route('addToCart', ['id' => $product->id])}}"><span class="dot"><span class="inner-dot"><i class="fa fa-plus"></i></span></span></a>
