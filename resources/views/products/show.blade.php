26095303c75a27f242c12006f02bca1c55474b44
Theus <theusdezeeuw@outlook.com>
2021-01-19 13:41:24 +0100

    Update show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,7 +1,7 @@
 <!-- Head -->
 @extends('includes/layout')
 
-@section('content')
+<!-- @section('content')
 
     <div class="container ">
         {{-- product name --}}
@@ -29,4 +29,40 @@
             </div>
         </div>
     </div>
+@stop -->
+
+@section('content')
+
+<div class="container-fluid">
+    <div class="content-wrapper">	
+		<div class="item-container">	
+			<div class="container">	
+				<div class="col-md-12">
+					<div class="product col-md-3 service-image-left">
+                    
+						<center>
+							<img id="item-display" src="/img/product_frame.png" alt=""></img>
+						</center>
+					</div>
+				</div>
+					
+				<div class="col-md-7">
+					<div class="product-title">{{$product->name}}</div>
+					<div class="product-desc">{{$product->description}}</div>
+					<div class="product-rating"><i class="fa fa-star gold"></i> <i class="fa fa-star gold"></i> <i class="fa fa-star gold"></i> <i class="fa fa-star gold"></i> <i class="fa fa-star-o"></i> </div>
+					<hr>
+					<div class="product-price">€ {{$product->price}}</div>
+					<div class="product-stock">In Stock</div>
+					<hr>
+					<div class="btn-group cart">
+						<button type="button" class="btn btn-success">
+							Add to cart 
+						</button>
+					</div>
+				</div>
+			</div> 
+		</div>
+	</div>
+</div>
+
 @stop
