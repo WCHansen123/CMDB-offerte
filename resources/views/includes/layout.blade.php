991febc5a93225270b5d233c8bf288ba8e1135f2
Theus <theusdezeeuw@outlook.com>
2021-01-19 10:07:20 +0100

    Update layout.blade.php

--- resources/views/includes/layout.blade.php
+++ resources/views/includes/layout.blade.php
@@ -8,11 +8,9 @@
 
     <title>CMDB-Offerte | Home</title>
     <!-- Styles -->
-    <link rel="stylesheet" href="{{URL::asset('../css/productscards.css')}}">
     <link rel="stylesheet" href="{{URL::asset('../css/nav.css')}}">
-    <link rel="stylesheet" href="{{URL::asset('../css/contact.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('../css/productscards.css')}}"> 
     <link rel="stylesheet" href="{{URL::asset('../css/productshow.css')}}">
-    <link rel="stylesheet" href="{{URL::asset('../css/aboutus-section.css')}}">
 
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous"></link>
